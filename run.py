3e364c0ed663b526c31c5717da564ffac3b2df2c
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-02-21 10:49:34 +0000

    Feat: add function to display memory field

--- run.py
+++ run.py
@@ -12,6 +12,23 @@ def generate_memory_field():
     random.shuffle(list_dump)
     return(list_dump)
 
+def display_memory_field(memory_field):
+    """
+    Generate a string out of the memory field and
+    displays it on the screen
+    """
+    row=1
+    memory_string=""
+    print("  A B C D E")
+    for x in range(len(memory_field)):
+        if x%5==0:
+            memory_string+=f"{row} {memory_field[x]} "
+            row+=1
+        elif x%5==4:
+            memory_string+=f"{memory_field[x]}\n"
+        else:
+            memory_string+=f"{memory_field[x]} "
+    print(memory_string)
 
 def main():
     """
@@ -19,6 +36,6 @@ def main():
     """
     print("Welcome to the memory card game!")
     memory = generate_memory_field()
-    print(memory)
+    display_memory_field(memory)
 
 main()
