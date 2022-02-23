3fd9256654ce973acabd45d637a37261b96ca84c
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-02-23 14:21:58 +0000

    Style: made code and comments easier to read and removed blank spaces

--- run.py
+++ run.py
@@ -1,23 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random   # needed for shuffling of the deck
+import os       # needed for clearing the terminal screen
 
-import random
-import os
 
 class MemoryCard():
     """
     Creates an instance of MemoryCard
     """
     def __init__(self):
-        self.score = 0                                      # keeps track of the score, 10=all cards revealed
-        self.fails = 0                                      # keeps track of unsuccessful tries
-        self.solution = ["0", "0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5",
-                 "6", "6", "7", "7", "8", "8", "9", "9"]
+        self.score = 0     # keeps track of the score, 10=all cards revealed
+        self.fails = 0     # keeps track of unsuccessful tries
+        self.solution = ["0", "0", "1", "1", "2", "2", "3", "3", "4", "4",
+                         "5", "5", "6", "6", "7", "7", "8", "8", "9", "9"]
         self.guess = ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
                       "X", "X", "X", "X", "X", "X", "X", "X", "X"]
-        random.shuffle(self.solution)                       # shuffles the list to create a random Card Deck
-        
+        # shuffles the list to create a random Card Deck
+        random.shuffle(self.solution)
+
     def is_revealed(self, index):
         """
         Checks if the Card is already revealed on the field
@@ -26,6 +24,7 @@ class MemoryCard():
             return True
         else:
             return False
+
 
 def display_memory_field(memory_field):
     """
@@ -37,122 +36,179 @@ def display_memory_field(memory_field):
     memory_string = ""
     print("  A B C D E")
     for memories in memory_field:
-        if column % 5==0:                           #start of a new row, prints row index then current card
+        # start of a new row, prints row index then current card
+        if column % 5 == 0:
             memory_string += f"{row} {memories} "
             row += 1
-        elif column % 5==4:                         #prints current card then ends the current row after the 5th card
+        # prints current card then ends the current row after the 5th card
+        elif column % 5 == 4:
             memory_string += f"{memories}\n"
-        else:                                       #prints out current card
+        # prints out current card
+        else:
             memory_string += f"{memories} "
         column += 1
 
     print(memory_string)
-    
+
+
 def validate_input(input_str):
     """
     Checks if Input is valid
     """
-    if len(input_str) == 2:                         #checks if input lenght is exactly 2
-        list = [char for char in input_str]         #splits the string into 2 elements
-        list[0] = list[0].upper()                   #makes the first letter uppercase
-        
-        if list[0] == "A" or list[0] == "B" or list[0] == "C" or list[0] == "D" or list[0] == "E":
-            if(list[1].isdigit()):                              #checks if second is a digit and above checks if first is between A and E
-                if(int(list[1]) > 0 and int(list[1]) < 5):      #checks if between 1 and 4
+    # checks if input lenght is exactly 2
+    if len(input_str) == 2:
+        # splits the string into 2 elements
+        characters = [char for char in input_str]
+        # makes the first letter uppercase
+        characters[0] = characters[0].upper()
+
+        # checks if first letter is between A and E
+        if characters[0] == "A" or characters[0] == "B" or characters[0] == "C" or characters[0] == "D" or characters[0] == "E":
+            # checks if second letter is a digit
+            if(characters[1].isdigit()):
+                # checks if between 1 and 4
+                if(int(characters[1]) > 0 and int(characters[1]) < 5):
                     return True
                 else:
-                    return False                    #False is returned in all instances of an invalid input
+                    # False is returned in all instances of an invalid input
+                    return False
             else:
                 return False
         else:
             return False
     else:
-        return False        
+        return False
+
 
 def convert_to_index(input_str):
     """
     Converts Input into correct Index for List
     """
-    list = [char for char in input_str]             #splits the string into 2 elements
+    # splits the string into 2 elements
+    characters = [char for char in input_str]
     index = 0
-    if list[0].upper() == "A":                      #increments the index for columns
+    # increments the index for columns
+    if characters[0].upper() == "A":
         index += 0
-    elif list[0].upper() == "B":
+    elif characters[0].upper() == "B":
         index += 1
-    elif list[0].upper() == "C":
+    elif characters[0].upper() == "C":
         index += 2
-    elif list[0].upper() == "D":
+    elif characters[0].upper() == "D":
         index += 3
-    elif list[0].upper() == "E":
+    elif characters[0].upper() == "E":
         index += 4
     else:
-        return 99                                   #returns 99 in case of an exception
+        # returns 99 in case of an exception
+        return 99
 
-    index += (int(list[1])-1)*5                     #increments index for rows (row 1=0, row 2=5, row 3=10 and row 4=15)
+    # increments index for rows (row 1=0, row 2=5, row 3=10 and row 4=15)
+    index += (int(characters[1])-1)*5
 
-    return index                                    #returns index number
+    return index       # returns index number
 
 
-def main():                                         
+def main():
     """
     Runs the application
     """
-    print("Welcome to the memory card game!")                               #Welcome message and instructions
+    # Welcome message and instructions
+    print("Welcome to the memory card game!")
     print("In this Game, you have to find the pair of cards that match.")
     print("Reveal the whole Deck and you win.")
-    playthegame = MemoryCard()                                              #creates a new instance of the class, creating the game
+
+    # creates a new instance of the class, creating the game
+    playthegame = MemoryCard()
     display_memory_field(playthegame.solution)
-    
+
     while playthegame.score < 10:
-        while True:                                                             #starts the first selection of the card
+        # starts the first selection of the card
+        while True:
             print("Please chose the first Card to reveal in the format")
             print("Column and then Row (e.g 'A1' or 'C2' or 'E3')\n")
             display_memory_field(playthegame.guess)
             cardone_str = input("Which Card do you want to reveal? ")
-            if(validate_input(cardone_str)):                                    #validates if input is in correct format
+
+            # validates if input is in correct format
+            if validate_input(cardone_str):
                 print("Your Input was valid!")
-                index1 = convert_to_index(cardone_str)                           #converts the valid input into an index for the list
-                if index1 == 99:                                                 #exception handling, should never happen
+
+                # converts the valid input into an index for the list
+                index1 = convert_to_index(cardone_str)
+
+                # exception handling, should never happen
+                if index1 == 99:
                     print("You shouldn't be here!")
-                elif playthegame.is_revealed(index1):                            #checks if list is already revealed on the field
+
+                # checks if list is already revealed on the field
+                elif playthegame.is_revealed(index1):
                     print("The card is already revealed on the field!")
+
+                # tells the player which card he/she revealed
                 else:
-                    print(f"The Card you reveal is: {playthegame.solution[index1]}")     #tells the player which card he/she revealed
+                    print(f"The Card you reveal is: {playthegame.solution[index1]}")
                     break
             else:
                 print("Invalid Input!")
-        
-        while True:                                                             #starts the second selection of the card
+
+        # starts the second selection of the card
+        while True:
             print("Please chose the second Card to reveal in the format")
             print("Column and then Row (e.g 'A1' or 'C2' or 'E3')\n")
             cardtwo_str = input("Which Card do you want to reveal? ")
-            if(validate_input(cardtwo_str)):                                    #validates if input is in correct format
+
+            # validates if input is in correct format
+            if validate_input(cardtwo_str):
                 print("Your Input was valid!")
-                index2 = convert_to_index(cardtwo_str)                          #converts the valid input into an index for the list
-                if index2 == 99:                                                #exception handling, should never happen
+
+                # converts the valid input into an index for the list
+                index2 = convert_to_index(cardtwo_str)
+
+                # exception handling, should never happen
+                if index2 == 99:
                     print("You shouldn't be here!")
-                elif index2 == index1:                                          #checks if player chose same card with both inputs
-                    print("You have to chose 2 different cards to reveal!")     
-                elif playthegame.is_revealed(index2):                           #checks if list is already revealed on the field
+
+                # checks if player chose same card with both inputs
+                elif index2 == index1:
+                    print("You have to chose 2 different cards to reveal!")
+
+                # checks if list is already revealed on the field
+                elif playthegame.is_revealed(index2):
                     print("The card is already revealed on the field!")
+
+                # tells the player which card he/she revealed
                 else:
-                    print(f"The Card you reveal is: {playthegame.solution[index2]}")    #tells the player which card he/she revealed
+                    print(f"The Card you reveal is: {playthegame.solution[index2]}")
                     break
             else:
                 print("Invalid Input!")
-        
-        if playthegame.solution[index1] == playthegame.solution[index2]:        #checks if both revealed cards are the same
+
+        # checks if both revealed cards are the same
+        if playthegame.solution[index1] == playthegame.solution[index2]:
             print("You managed to reveal a pair!")
-            playthegame.guess[index1] = playthegame.solution[index1]            #updates the guess list with revealed cards
+
+            # updates the guess list with revealed cards
+            playthegame.guess[index1] = playthegame.solution[index1]
             playthegame.guess[index2] = playthegame.solution[index2]
-            playthegame.score += 1                                              #increases gamescore (correct guesses) by 1
-        
-        else:                                                                   #chosen cards don't match
+
+            # increases gamescore (correct guesses) by 1
+            playthegame.score += 1
+
+        # chosen cards don't match
+        else:
             print("The two cards don't match...")
-            playthegame.fails += 1                                              #increases wrong guesses by 1
-        
-        input("Press Enter to continue...")                                     #waits for player to press Enter to continue the game
-        os.system('cls||clear')                                                 #clears the gamescreen before the next round, this prevents the player
-                                                                                #from scrolling up and looking for old reveals
+
+            # increases wrong guesses by 1
+            playthegame.fails += 1
+
+        # waits for player to press Enter to continue the game
+        input("Press Enter to continue...")
+
+        # clears the gamescreen before the next round, this prevents the player
+        # from scrolling up and looking for old reveals
+        os.system('cls||clear')
+
     print("Good Job! You finished the whole Deck!")
+
+
 main()
